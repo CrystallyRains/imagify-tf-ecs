554f69f70f9c3b9f7436cad7426f838ea8c15b1e
CrystallyRains <snigdhachaudhari1@gmail.com>
2026-03-05 20:12:07 +0530

    updates

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-   bucket         = "imagify-terraform-state"
-    key            = "terraform/imagify.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "imagify-terraform-lock"
-  }
+  #backend "s3" {
+   # bucket         = "imagify-terraform-state"
+   # key            = "terraform/imagify.tfstate"
+   # region         = "us-east-1"
+   # dynamodb_table = "imagify-terraform-lock"
+ # }
 }
 
